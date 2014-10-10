10e8be27c831d036875d04635a3ef0072e559220
Gang Chen <chengangcs@gmail.com>
2014-10-11 00:45:50 +0800

    Development of Perl module

---
+++ GNBF5010-5/PUBMED.pm
@@ -0,0 +1,41 @@
+package PUBMED;
+
+=head
+Scrape publication titles according to PubMed ids.
+
+For CUHK-I2P course.
+
+Usage:
+  perl lwp.pl filename
+
+Argument:
+  filename: a file of PubMed ids. Each line is a PubMed id.
+
+Author: Gang Chen
+=cut
+
+use warnings;
+use strict;
+
+use LWP::Simple;
+
+sub getTitles($){
+
+  my $filename = shift;
+
+  open my $idFile, $filename or die $!;
+
+  while(<$idFile>){
+
+    chomp;
+
+    my $content = get("http://www.ncbi.nlm.nih.gov/pubmed/".$_);
+
+    $content =~ m/<h1>(.+)<\/h1>/;
+
+    print $_,"\t",$1,"\n";
+  }
+  close $idFile;
+}
+
+1;
---
+++ GNBF5010-5/run.pl
@@ -0,0 +1,3 @@
+use PUBMED
+
+PUBMED::getTitles("pubmedids.txt");
