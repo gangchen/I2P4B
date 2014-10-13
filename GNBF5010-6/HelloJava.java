e0d0273c2748b8c90268eeb6c5b07376c6c76b3d
Gang Chen <chengangcs@gmail.com>
2014-10-13 16:22:39 +0800

    Lec 6

---
+++ GNBF5010-6/HelloJava.java
@@ -0,0 +1,15 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloJava{
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+}
