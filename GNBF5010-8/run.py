c053d5d85c296405c5d369288956b54b571d7a4c
Gang Chen <chengangcs@gmail.com>
2014-11-01 15:40:49 +0800

    perl and python scripts are added

---
+++ GNBF5010-8/run.py
@@ -0,0 +1,16 @@
+a = 1.3
+b = 0.7
+c = 0.6
+
+strA = "Hello"
+strB = "Python"
+
+
+print a-b
+
+print strA + " " + strB
+
+if a - b == c:
+    print "1.3 - 0.7 is equal to 0.6"
+else:
+    print "1.3 - 0.7 is not equal to 0.6"
