791d69b7a2febf6d7e1242e1066920101baca79a
Gang CHEN <chengangcs@gmail.com>
2015-10-16 23:58:02 +0800

    struct refine for Perl classes

---
+++ GNBF5010-10-Other/Run.java
@@ -0,0 +1,8 @@
+public class Run{
+  public static void main(String[] args) {
+
+    int a;
+
+    System.out.println(a);
+  }
+}
--- GNBF5010-4-Perl/hello.pl
+++ GNBF5010-4-Perl/hello.pl
@@ -1,11 +1,1 @@
-#!/usr/bin/perl
-
-=hello
-Hello example for GNBF5010
-Author: Gang Chen
-=cut
-
-use warnings;
-use strict;
-
 print "Hello, Perl!\n";
