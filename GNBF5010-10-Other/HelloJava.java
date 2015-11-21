185d75bf3ec25668ba643668f450102c0b05f0f2
Gang CHEN <chengangcs@gmail.com>
2015-11-21 10:55:43 +0800

    lecture 10

--- GNBF5010-10-Other/HelloJava.java
+++
@@ -1,8 +0,0 @@
-
-public class HelloJava{
-
-    public static void main(String[] args) {
-        System.out.println("Hello Java!");
-    }
-
-}
--- GNBF5010-10-Other/Run.java
+++
@@ -1,8 +0,0 @@
-public class Run{
-  public static void main(String[] args) {
-
-    int a;
-
-    System.out.println(a);
-  }
-}
